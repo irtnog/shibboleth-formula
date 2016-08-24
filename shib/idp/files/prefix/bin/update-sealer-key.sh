d7d19ccd85141619cbed4dc352125b09dc063d6e
Matthew X. Economou <xenophon@irtnog.org>
2016-08-24 09:36:13 -0400

    fix(shib.idp): move the sealer password to a separate, protected file
    Unfortunately, the Shibboleth IdP resets the permissions of the sealer
    key update script, making it world-readable.  This change moves the
    sealer password out of the script and into a file with restricted
    permissions.

--- shib/idp/files/prefix/bin/update-sealer-key.sh
+++ shib/idp/files/prefix/bin/update-sealer-key.sh
@@ -6,8 +6,11 @@ JAVA_HOME=$(java -XshowSettings:java.home -version 2>&1 \
 		| awk '{print $3}')
 export JAVA_HOME
 
+SEALER_PASSWORD=$(cat {{ shibidp_settings.prefix }}/credentials/sealer.pass)
+export SEALER_PASSWORD
+
 {{ shibidp_settings.prefix }}/bin/seckeygen.sh \
     --storefile {{ shibidp_settings.prefix }}/credentials/sealer.jks \
-    --storepass {{ shibidp_settings.sealer_password|yaml_squote }} \
+    --storepass "${SEALER_PASSWORD}" \
     --versionfile {{ shibidp_settings.prefix }}/credentials/sealer.kver \
     --alias secret
