1c0d041387d9beea417f19827f62d6be1fcf6744
Matthew X. Economou <xenophon@irtnog.org>
2016-08-26 10:14:41 -0400

    fix(shibboleth.idp): build script wrapper should probably specify idp.home

--- shibboleth/idp/files/build.sh
+++ shibboleth/idp/files/build.sh
@@ -6,4 +6,4 @@ JAVA_HOME=$(java -XshowSettings:java.home -version 2>&1 \
 		| awk '{print $3}')
 export JAVA_HOME
 
-{{ shibidp_settings.prefix }}/bin/build.sh -Didp.noprompt=1
+{{ shibidp_settings.prefix }}/bin/build.sh -Didp.home={{ shibidp_settings.prefix|yaml_squote }} -Didp.noprompt=1
